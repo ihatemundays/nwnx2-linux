323dd87f58e3f8a2718c54ae61003b0f57463a0b
Scott Munday <smunday@knowware.org>
2017-07-24 22:12:17 -0400

    Putting error handling over most issues.

--- plugins/neo4j/NWNXNeo4j.cpp
+++ plugins/neo4j/NWNXNeo4j.cpp
@@ -12,26 +12,30 @@ CNWNXNeo4j::~CNWNXNeo4j() {
 
 bool CNWNXNeo4j::OnCreate(gline *config, const char* logDirectory)
 {
-    char log[128];
+    try {
+        char log[128];
 
-    sprintf(log, "%s/nwnx_neo4j.txt", logDirectory);
-    if (!CNWNXBase::OnCreate(nwnxConfig, log)) {
-        return false;
+        sprintf(log, "%s/nwnx_neo4j.txt", logDirectory);
+        if (!CNWNXBase::OnCreate(nwnxConfig, log)) {
+            return false;
+        }
+
+        Log(0, "NWNX Neo4j V.1.0\n");
+        Log(0, "(c) 2017 by Wayne Chipchase / Scott Munday\n");
+        Log(0, "* Module loaded successfully.\n");
+
+        if (!LoadConfiguration()) {
+            return false;
+        }
+
+        if (!Connect()) {
+            return false;
+        }
+
+        return true;
+    } catch (exception const& error) {
+        cerr << "Unable to create Neo4j plugin. " << error.what() << endl;
     }
-
-    Log(0, "NWNX Neo4j V.1.0\n");
-    Log(0, "(c) 2017 by Wayne Chipchase / Scott Munday\n");
-    Log(0, "* Module loaded successfully.\n");
-
-    if (!LoadConfiguration()) {
-        return false;
-    }
-
-    if (!Connect()) {
-        return false;
-    }
-
-    return true;
 }
 
 bool CNWNXNeo4j::LoadConfiguration() {
@@ -104,16 +108,20 @@ void CNWNXNeo4j::Disconnect() {
 }
 
 char* CNWNXNeo4j::OnRequest(char* gameObject, char* request, char* arguments) {
-    Log(2, "Request: \"%s\"\n", request);
-    Log(3, "Arguments:  \"%s\"\n", arguments);
+    try {
+        Log(2, "Request: \"%s\"\n", request);
+        Log(3, "Arguments:  \"%s\"\n", arguments);
 
-    if (strcmp(request, "EXEC") == 0) {
-        Exec(arguments);
-    } else if (strcmp(request, "FETCH") == 0) {
-        return Fetch(arguments, strlen(arguments));
+        if (strcmp(request, "EXEC") == 0) {
+            Exec(arguments);
+        } else if (strcmp(request, "FETCH") == 0) {
+            return Fetch(arguments, strlen(arguments));
+        }
+
+        return NULL;
+    } catch (exception const& error) {
+        cerr << "Error on Neo4j request. " << error.what() << endl;
     }
-
-    return NULL;
 }
 
 bool CNWNXNeo4j::OnRelease () {
