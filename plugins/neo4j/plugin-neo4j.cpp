6d6be35c927ebf697415842613c56bfd80cd1da6
Scott Munday <smunday@knowware.org>
2017-07-25 19:53:47 -0400

    Adding all aspects that are part of plugins that use ini file.

--- plugins/neo4j/plugin-neo4j.cpp
+++ plugins/neo4j/plugin-neo4j.cpp
@@ -1,10 +1,34 @@
 #include "NWNXNeo4j.h"
 
 CNWNXNeo4j neo4j;
+PLUGINLINK *pluginLink = 0;
 
-extern "C" {
-    CNWNXBase* GetClassObject()
-    {
-        return &neo4j;
-    }
+PLUGININFO pluginInfo = {
+        sizeof(PLUGININFO),
+        "NWNXNeo4j",
+        PLUGIN_MAKE_VERSION(0, 0, 0, 1),
+        "",
+        "Wayne Chipchase / Scott Munday",
+        "",
+        "MIT",
+        "",
+        0
+};
+
+extern "C" PLUGININFO* GetPluginInfo(DWORD nwnxVersion)
+{
+    return &pluginInfo;
+}
+
+extern "C" int InitPlugin(PLUGINLINK *link)
+{
+    pluginLink = link;
+
+    return 0;
+}
+
+extern "C"
+CNWNXBase* GetClassObject()
+{
+    return &neo4j;
 }
